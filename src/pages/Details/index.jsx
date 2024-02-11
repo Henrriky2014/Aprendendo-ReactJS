4cd805db283fa2390c61b1a2de56d4465297beed
CH <carloshenrriky1@gmail.com>
2024-02-11 20:32:30 -0300

    Add vite and styled-components and Overall Style

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from './styles';
+
+import { Button } from '../../components/Button'
+
+
+export function Details() {
+  return(
+    <Container>
+      <h1>Hello World</h1>
+      <span>Carlos Henrriky</span>
+
+      <Button/>
+
+    </Container>
+  )
+}
