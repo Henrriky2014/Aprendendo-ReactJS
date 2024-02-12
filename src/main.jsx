456fd139b310d8c7b365b045873d5a90827b65d9
CH <carloshenrriky1@gmail.com>
2024-02-12 16:26:51 -0300

    Add Profile page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalSyles from './styles/global'
 
 import theme from './styles/theme';
 
-import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalSyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
