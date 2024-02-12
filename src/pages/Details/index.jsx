c858bdec3de1bcefe3738e9cb9323e9be0fe1e13
CH <carloshenrriky1@gmail.com>
2024-02-12 07:44:09 -0300

    Add Head and Markers and Links

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,29 @@
-import { Container } from './styles';
+import { Container, Links } from './styles';
 
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 
 export function Details() {
   return(
     <Container>
-      <h1>Hello World</h1>
-      <span>Carlos Henrriky</span>
+      <Header/>
 
-      <Button/>
+      <Section title="Links Úteis">
+        <Links>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="nodejs"/>
+      </Section>
+
+      <Button title="Voltar"/>
 
     </Container>
   )
