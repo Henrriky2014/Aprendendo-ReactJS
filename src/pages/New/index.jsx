c35ff763c8b07d688cd829e81b6ab6cda224b94a
CH <carloshenrriky1@gmail.com>
2024-02-18 15:28:53 -0300

    Add functionality to remove link button

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -4,12 +4,28 @@ import { Section } from '../../components/Section';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
 
 import { Container, Form } from './styles';
 
 export function New() {
+    const [links, setLinks] = useState([]);
+    const [newLink, setNewLink] = useState("");
+
+    function handleAddLink() {
+        setLinks(prevState => [...prevState, newLink])
+        setNewLink("");
+    }
+
+    function handleRemoveLink(deleted) {
+        setLinks(prevState => prevState.filter(link => link !== deleted))
+    }
+    
+    
     return(
         <Container>
             <Header />
@@ -25,8 +41,23 @@ export function New() {
                     <Textarea placeholder= "Observações"/>
 
                     <Section title="Links Úteis">
-                        <NoteItem value="https://rocketseat.com.br"/>
-                        <NoteItem placeholder="Novo Link" isNew/>
+                        {
+                            links.map((link, index) => (
+                                <NoteItem
+                                    key={String(index)}
+                                    value={link}
+                                    onClick={() => {handleRemoveLink(link)}}
+                                 />
+                            ))
+                        }
+                        
+                        <NoteItem 
+                            isNew
+                            placeholder="Novo Link" 
+                            value={newLink}
+                            onChange={e => setNewLink(e.target.value)}
+                            onClick={handleAddLink}
+                        />
                     </Section>
 
                     <Section title="Marcadores">
